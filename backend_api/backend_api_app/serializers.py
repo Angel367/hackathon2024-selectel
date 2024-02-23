a49fa6e04f2ae7faac14555d006907404941bc62
Egor Volkov <egor.volkov20038@gmail.com>
2024-02-23 15:44:25 +0300

    auth + reg xx4

--- backend_api/backend_api_app/serializers.py
+++ backend_api/backend_api_app/serializers.py
@@ -83,7 +83,8 @@ class LoginSerializer(serializers.Serializer):
         return {
             'email': user.email,
             'username': user.username,
-            'token': user.token
+            'token': user.token,
+            'id': user.id
         }
 
 
