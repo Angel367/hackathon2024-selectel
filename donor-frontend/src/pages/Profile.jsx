b21a491256d520c15acc92622fdd91e8669bb101
13melisa13 <isaeva.a.v@edu.mirea.ru>
2024-02-24 18:31:24 +0300

    user_ro_profile

--- donor-frontend/src/pages/Profile.jsx
+++ donor-frontend/src/pages/Profile.jsx
@@ -1,50 +1,55 @@
 import { Link, useParams } from "react-router-dom";
 import DonorButton from "../components/DonorButton";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 import { readUser } from "../api/user_for_profile";
+import axios from "axios";
 export default function Profile() {
   const navigate = useNavigate();
   const { username } = useParams();
   const user = JSON.parse(localStorage.getItem("user"));
-  let list_of_components = [];
-  if (user.user.ready_to_donate_blood) {
+    const [donorInfo, setDonorInfo] = useState([]);
+  const fetchData = () => {
+   readUser(user.user.token)
+      .then((response) => {
+        setDonorInfo(response.data);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+    let list_of_components = [];
+    // console.log(donorInfo.user.donor-card, "donorInfo");
+  if (donorInfo.user.ready_to_donate_blood) {
     list_of_components.push("цельную кровь");
   }
-  if (user.user.ready_to_donate_platelets) {
+  if (donorInfo.user.ready_to_donate_platelets) {
     list_of_components.push("тромбоциты");
   }
-  if (user.user.ready_to_donate_erythrocytes) {
+  if (donorInfo.user.ready_to_donate_erythrocytes) {
     list_of_components.push("эритроциты");
   }
-  if (user.user.ready_to_donate_granulocytes) {
+  if (donorInfo.user.ready_to_donate_granulocytes) {
     list_of_components.push("гранулоциты");
   }
-  if (user.user.ready_to_donate_plasma) {
+  if (donorInfo.user.ready_to_donate_plasma) {
     list_of_components.push("плазму");
   }
 
-  const contactDataList = [
-    user.user.email,
-    user.user.phone_number,
-    user.user.first_name + " " + user.user.last_name,
+
+  console.log(donorInfo, "donorInfo");
+const contactDataList = [
+    donorInfo.user.first_name + " " +donorInfo.user.last_name,
+    donorInfo.user.email,
+    donorInfo.user.phone_number,
   ];
-
-  let donorInfo;
-  useEffect(() => {
-    const getDonorInfo = async () => {
-      donorInfo = await readUser(username);
-    };
-
-    getDonorInfo();
-  }, []);
-
-  console.log(donorInfo);
-
   return (
     <div className="container">
-      <h4>{username}</h4>
-      {/*<h4>{(user.user.first_name || "")+ " " + (user.user.last_name || "")}</h4>*/}
+
+      <h4>{(donorInfo.user.first_name || "")+ " " + (user.user.last_name || "")}</h4>
       <label htmlFor="contacts">
         Персональные данные
         <div className="input_container">{contactDataList.join(", ")}</div>
@@ -52,22 +57,22 @@ export default function Profile() {
       <label htmlFor="donor-card">
         Карточка донора
         <div className="input_container"></div>
-        {/* <input
+         <input
           id="donor-card"
           type="text"
           disabled
           value={
-            (user.user.blood_group || "") +
+            (donorInfo.user.blood_group || "") +
             " " +
-            (user.user.rh_factor || "") +
+            (donorInfo.user.rh_factor || "") +
             ", " +
             "Kell-фактор" +
-            (user.user.kell_factor || "") +
+            (donorInfo.user.kell_factor || "") +
             "/n" +
             "готов сдать " +
             list_of_components.map((components) => components + ", ")
           }
-        /> */}
+        />
       </label>
       <label htmlFor="plan-donatation">
         Напомнить о донации
@@ -75,7 +80,7 @@ export default function Profile() {
           Укажите центр крови, планируемую дату и тип донации.За 3 дня до
           намеченной даты мы пришлём напоминание на электронную почту.
         </div>
-        {/* <input
+        <input
           id="plan-donatation"
           type="text"
           disabled
@@ -86,7 +91,7 @@ export default function Profile() {
             "Дата" +
             (user.user.plan_donations_last.donation_date || "")
           }
-        /> */}
+        /> *
       </label>
       <DonorButton
         text="Запланировать"
